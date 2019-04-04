6bbe43b8462ef063381decaa1f661514459c4380
Roman <romannikulin1@yandex.ru>
2019-04-04 22:20:55 +0300

    Title page added

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
@@ -11,6 +10,3 @@ db = SQLAlchemy(app)
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'), 404
-
-from app.users.views import mod as usersModule
-app.register_blueprint(usersModule)
