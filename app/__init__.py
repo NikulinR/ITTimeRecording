1fd0815853f26beba54239a0237985766bd580c6
NikulinR <RomanNikulin1@yandex.ru>
2019-04-10 19:25:51 +0300

    orm started
    Исправлена функциональная схема, добавлены модели для SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 import sqlite3
 from flask import Flask, redirect, render_template, session, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'gjr39dkjn344_!67#'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'timerec.db')
 db = SQLAlchemy(app)
 
 
