e37cc59755b2d6d98873b4c995e47107e7057f5b
Mikheey <mikh.dryomin@yandex.ru>
2019-04-22 16:30:36 +0300

    Fix

--- rec/mycalendar.py
+++ rec/mycalendar.py
@@ -8,7 +8,7 @@ class Day:
 
 class Calendar:
     months_names = {
-        1: 'January', 2: 'Feburary', 3: 'March', 4: 'April',
+        1: 'January', 2: 'February', 3: 'March', 4: 'April',
         5: 'May', 6: 'June', 7: 'July', 8: 'August',
         9: 'September', 10: 'October', 11: 'November', 12: 'December'
     }
